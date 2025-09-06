1fa9bec7f91be8e59ab19b48be2b199ffb5bb09b
VilMe <vilma.medrano00@gmail.com>
2025-09-05 22:27:09 -0500

    define main function previously defined roll_dice function

--- main.py
+++ main.py
@@ -13,3 +13,14 @@ def roll_dice(amount: int = 2) => list[int]:
 
     return rolls
 
+def main():
+    while True;
+        try: 
+            user_input: str = input('How many dice would you like to roll? ')
+
+            if user_input.lower() == 'exit':
+                print('Thanks for playing!')
+                break
+            print(roll_dice(int(user_input)))
+        except ValueError:
+            print('(Please enter a valid number!!)')
