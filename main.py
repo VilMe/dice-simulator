c871086982d26e49fcd27f5fa365875331a2bddd
VilMe <vilma.medrano00@gmail.com>
2025-09-07 22:38:39 -0500

    add sum of all dice values at the end in parentheses

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ def roll_dice(amount: int = 2) -> list[int]:
     for i in range(amount):
         random_roll: int = random.randint(1, 6)
         rolls.append(random_roll)
-
-    return rolls
+    sum_of_rolls = sum(rolls)
+    return rolls, sum_of_rolls
 
 def main():
     while True:
@@ -21,7 +21,10 @@ def main():
             if user_input.lower() == 'exit':
                 print('Thanks for playing!')
                 break
-            print(*roll_dice(int(user_input)), sep=', ')
+            # print(*roll_dice(int(user_input)), sep=', ')
+            dice_roll = roll_dice(int(user_input))
+            print(*dice_roll[0], sep=', ', end=" ") 
+            print(f'({dice_roll[1]})')
         except ValueError:
             print('(Please enter a valid number!!)')
 
